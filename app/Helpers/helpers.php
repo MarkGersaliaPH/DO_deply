a2549a8c8a4a3844e01e1eec9ce8cdabdd88e955
Mark Anthony Gersalia <markgersalia.codev@gmail.com>
2019-06-27 16:39:58 +0800

    Fix helpers

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,1 +1,86 @@
- 
+<?php 
+
+
+
+function getStatus($status){
+    $class ='';
+    switch ($status) {
+        case 'Pending':
+            # code...
+            $class = 'warning';
+            break;
+        case 'Completed':
+            # code...
+            $class = 'success';
+            break;
+        
+        case 'On going':
+            # code...
+            $class = 'primary';
+            break;
+        
+        
+        case 'For QA Testing':
+            # code...
+            $class = 'info';
+            break;
+        
+        default:
+            # code...
+            break;
+    }
+
+    return $class;
+}
+
+function getPriorityClass($priority){
+    $class ='';
+    
+    switch ($priority) {
+        case 'High':
+            # code...
+            $class = 'danger';
+            break;
+        case 'Medium':
+            # code...
+            $class = 'warning';
+            break;
+        
+        case 'Low':
+            # code...
+            $class = 'bg-white';
+            break;
+        
+        default:
+            # code...
+            break;
+    }
+
+    return $class;
+}
+
+function getPriorityIcon($priority){
+    $icon ='';
+
+    switch ($priority) {
+        case 'High':
+            # code...
+            $icon = '<i class="fas fa-skull-crossbones text-danger"></i>';
+            break;
+        case 'Medium':
+            # code...
+            $icon = '<i class="fas fa-exclamation-triangle text-warning "></i> ';
+            break;
+
+        case 'Low':
+            # code...
+            $icon = '<i class="fas fa-bookmark text-muted"></i> ';
+            break;
+
+        default:
+            # code...
+            break;
+    }
+
+    return $icon;
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,11 +11,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
-    {
-        //
+public function register()
+{
+    foreach (glob(app_path() . '/Helpers/*.php') as $file) {
+        require_once($file);
     }
-
+}
     /**
      * Bootstrap any application services.
      *
